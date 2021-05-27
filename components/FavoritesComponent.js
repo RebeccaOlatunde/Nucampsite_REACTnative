61ecdfb32a9c66db37298eca0064802dd690de14
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-05-27 19:28:42 -0400

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -30,10 +30,29 @@ class Favorites extends Component {
             return (
                 <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
                     <View style={styles.deleteView}>
-                        <TouchableOpacity
-                        style={styles.deleteTouchable}
-                        onPress={() => this.props.deleteFavorite(item.id)}
-                        >
+                    <TouchableOpacity
+                    style={styles.deleteTouchable}
+                    onPress={() =>
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite campsite ' +
+                                item.name +
+                                '?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log(item.name + 'Not Deleted'),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                },
+                            ],
+                            { cancelable: false }
+                        )
+                    }
+                >
                         <Text style={styles.deleteText}>Delete</Text>
                         </TouchableOpacity>
                     </View>
