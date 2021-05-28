730ca35f81d04f68f80bab4ceca29d4d73ab7009
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-05-27 21:51:53 -0400

    Gestures part 2b

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,10 +26,16 @@ function RenderCampsite(props) {
 
     const {campsite} = props;
 
+    const view = React.createRef();
+
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
@@ -55,12 +61,13 @@ function RenderCampsite(props) {
         }
     });
 
-    if (campsite) {
+    iif (campsite) {
         return (
             <Animatable.View
                 animation='fadeInDown'
                 duration={2000}
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}>
             <Card 
                 featuredTitle={campsite.name}
