a3512c965ababdc4986cb62bc1e08266d0ab78c7
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-06-03 09:42:57 -0400

    week 3 assignment

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+import { Text, View ,ScrollView,FlatList,Modal,Button,StyleSheet,Alert,PanResponder} from 'react-native';
 import { Card ,Icon,Rating,Input} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite,postComment } from '../redux/ActionCreators';
 import * as Animatable from 'react-native-animatable';
-
 
 const mapStateToProps = state => {
     return {
@@ -24,11 +22,13 @@ const mapDispatchToProps = {
 
 function RenderCampsite(props) {
 
-    const {campsite} = props;
-
+    const { campsite } = props;
+    
     const view = React.createRef();
 
-    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+    
+    const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -37,7 +37,7 @@ function RenderCampsite(props) {
             .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
         },
         onPanResponderEnd: (e, gestureState) => {
-            console.log('pan responder end', gestureState);
+            console.log('pan responder end', gestureState.dx);
             if (recognizeDrag(gestureState)) {
                 Alert.alert(
                     'Add Favorite',
@@ -54,8 +54,11 @@ function RenderCampsite(props) {
                                 console.log('Already set as a favorite') : props.markFavorite()
                         }
                     ],
-                    { cancelable: false }
+                    { cancelable: false}
                 );
+            }
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
@@ -68,36 +71,40 @@ function RenderCampsite(props) {
                 duration={2000}
                 delay={1000}
                 ref={view}
-                {...panResponder.panHandlers}>
-            <Card 
-                featuredTitle={campsite.name}
-                image={{uri: baseUrl + campsite.image}}>
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.favorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        raised
-                        reverse
-                        onPress={() => props.favorite ? 
-                                console.log('Already set as a favorite') : props.markFavorite()}
-                    />
-                    <Icon
-                        name='pencil'
-                        type='font-awesome'
-                        color='#5637DD'
-                        raised
-                        reverse
-                        onPress={()=>props.onShowModal()}
-                    />
-                </View>
+                {...panResponder.panHandlers}
+            >
+                <Card 
+                    featuredTitle={campsite.name}
+                    image={{uri: baseUrl + campsite.image}}
+                >
+                    <Text style={{margin: 10}}>
+                        {campsite.description}
+                    </Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.favorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={() => props.favorite ?
+                                    console.log('Already set as a favorite') : props.markFavorite()}
+                        />
+                        <Icon
+                            name='pencil'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={()=>props.onShowModal()}
+                        />
+                    </View>
                 </Card>
             </Animatable.View>
         );
     }
+
+    
     return <View />;
 }
 
@@ -121,13 +128,14 @@ function RenderComments({ comments }) {
         
     return (
         <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
-            <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}/>
+            <Card title="Comments">
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
             </Card>
-            </Animatable.View>
+        </Animatable.View>
     );
 }
 class CampsiteInfo extends Component {
