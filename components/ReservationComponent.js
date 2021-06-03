2fd098c4fb163423a11d1fc0c78f6ea211696871
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-06-03 13:45:06 -0400

    local notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button ,Alert} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+
 
 class Reservation extends Component{
     constructor(props) {
@@ -44,7 +46,7 @@ class Reservation extends Component{
                         {
                             text: 'OK',
                             onPress: () => {
-                                console.log("Ok was pressed")
+                                this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
                                 this.resetForm()
                             }
                         }
@@ -61,6 +63,32 @@ class Reservation extends Component{
                 showCalendar: false,
                 showModal:false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
